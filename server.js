42e686ba615f8e38d880e2741ad58a01adeed57d
tcultivator <whypanahon06@gmail.com>
2025-06-23 10:55:31 +0800

    Update server.js

--- server.js
+++ server.js
@@ -15,10 +15,10 @@ app.use(cookieparser())
 
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: 'mydb',
-    port: process.env.DB_PORT
+    database: process.env.DB_NAME
 })
 
 db.connect((err) => {
